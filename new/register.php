275bd368b8d675ef0a426b075ea34ecac03d9c38
Ssanaibanez <lsh2002@hotmail.com>
2018-04-25 17:53:37 -0400

    Add files via upload

---
+++ new/register.php
@@ -0,0 +1,44 @@
+<?php
+
+    // POST variables
+    //      $_POST["username"]      - user name
+    //      $_POST["email"]         - user email
+    //      $_POST["password"]      - user password
+
+    // Return
+    //          "message"           - success or error message
+
+    // connect to db
+    $host = 'us-cdbr-iron-east-05.cleardb.net';
+    $user = 'b90eb97b555c5e';
+    $pass = 'bdfd5fcd';
+    $db = 'heroku_ddc26ba47105536';
+    $mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
+
+    // Escape all $_POST variables to protect against SQL injections
+    $username = $mysqli->escape_string($_POST['username']);
+    $email = $mysqli->escape_string($_POST['email']);
+    $password = $mysqli->escape_string(password_hash($_POST['password'], PASSWORD_BCRYPT));
+
+    // Check if user with that email already exists
+    $result = $mysqli->query("SELECT * FROM users WHERE email='$email'") or die($mysqli->error());
+
+    // We know user email exists if the rows returned are more than 0
+    if ($result->num_rows > 0) {
+        $message = 'Failed to register. User with this email already exists!';
+    }
+    else { // Email doesn't already exist in a database, proceed...
+
+        // You will need 'hash' field if you want to add verification feature
+        $sql = "INSERT INTO users (username, email, password) " 
+                . "VALUES ('$username','$email','$password')";
+
+        // Add user to the database
+        if ($mysqli->query($sql)){
+            $message = "Thank you for signing up! Your account was created.";
+        }
+        else {
+            $message = 'Registration failed!';
+        }
+
+    }
